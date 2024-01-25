021f404015e79a98809818b105d7edeb97fb8e31
gkstnadl <gkstnadl12@naver.com>
2024-01-25 21:19:05 +0900

    Input 컴포넌트 만들고 기능 넣어두기

---
+++ src/components/InputArea.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+const TodoInput = ({ onTodo }) => {
+  const [inputs, setInputs] = useState({
+    title: '',
+    content: ''
+  });
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  const handleAddTodoClick = (e) => {
+    const { title, content } = inputs;
+    if (!title.trim() || !content.trim()) {
+      alert('제목과 내용을 모두 입력해주세요.');
+      return;
+    }
+    onTodo(inputs.title, inputs.content);
+    setInputs({ title: '', content: '' });
+  };
+
+  return (
+    <form>
+      <label>제목 : </label>
+      <input name='title' value={inputs.title} onChange={handleInput}></input>
+      <label>내용 : </label>
+      <input name='content' value={inputs.content} onChange={handleInput}></input>
+      <button onClick={handleAddTodoClick}>추가하기</button>
+    </form>
+  );
+};
